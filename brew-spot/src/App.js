32fa1437f094d8f5d8a97b055898f90a7b1f4861
Jesus Ortega <jortega23@Jesuss-MacBook-Pro.local>
2021-01-14 21:01:11 -0800

    WIP created search bar and added query search parameter

--- brew-spot/src/App.js
+++ brew-spot/src/App.js
@@ -2,30 +2,33 @@ import {useState, useEffect} from 'react';
 import axios from 'axios'
 import Header from './components/Header'
 import Display from './components/Display'
+import Search from './components/Search'
 import './App.css';
 
 function App() {
   //items/breweries
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState('')
 
 
   //fetching data from api
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        'https://api.openbrewerydb.org/breweries?per_page=5'
+        `https://api.openbrewerydb.org/breweries?by_city=${query}&per_page=5`
       )
       console.log(result.data)
       setItems(result.data)
     }
 
     fetchItems()
-  },[])
+  },[query])
 
 
   return (
     <div className="App">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <Display items={items}/>
     </div>
   );
